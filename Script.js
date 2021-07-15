448985569a319743aab20cfc0eab5033d21f4e3d
vineethpejathaya <vineethpejathaya@gmail.com>
2021-07-15 16:11:30 +0530

    commit the project

--- Script.js
+++ Script.js
@@ -1,1 +1,12 @@
-
+document.addEventListener('keydown',function(e){
+console.log(e.keyCode);
+let audio = document.querySelector(`audio[KeyCode="${e.keyCode}"]`);
+if(audio ===null)
+return;
+ let key=document.querySelector(`li[KeyBoard="${e.keyCode}"]`);
+ console.log(key);
+ key.classList.add('playing');
+ //key.classList.remove('playing');
+ audio.play();
+ audio.currentTime=0;
+});
